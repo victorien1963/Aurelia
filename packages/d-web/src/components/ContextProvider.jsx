acf4c326486b4413be49add59d8ecc4dd098d4aa
selikastraying <selikastraying@gmail.com>
2024-08-26 10:29:22 +0000

    fix page

--- packages/d-web/src/components/ContextProvider.jsx
+++ packages/d-web/src/components/ContextProvider.jsx
@@ -119,6 +119,7 @@ function ContextProvider(props) {
   }
   useEffect(() => {
     if (containerId) getTags()
+    else setTags([])
   }, [containerId])
 
   const handleAddTag = async (name, setting) => {
